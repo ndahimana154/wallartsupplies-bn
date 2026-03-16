427b4a53bf650966381098f121517c8e810f0d1a
Ndahimana Bonheur <ndahimana154@gmail.com>
2026-03-16 11:31:02 +0200

    SOlve the categories limit & add CI/CD (#10)

--- src/controllers/productsControllers.ts
+++ src/controllers/productsControllers.ts
@@ -136,7 +136,7 @@ const customerGetBestCategories = async (req: ExtendedRequest, res: Response): P
             {
                 sortBy: "updatedAt",
                 order: "DESC",
-                limit: 6
+
             });
         return sendSuccess(res, "Categories retrieved successfully", categories)
     } catch (error: any) {
